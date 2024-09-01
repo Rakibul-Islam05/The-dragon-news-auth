c7a9ef69f212b8633982487d1faef9d01d655651
Rakibul Islam <rakibulrakib05@gmail.com>
2024-09-01 10:24:13 +0600

    project router,tailwind,font,homePage and date Formatting setup

---
+++ src/pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LeftSideNav = () => {
+    return (
+        <div>
+            <h3 className="text-xl">LeftSide</h3>
+        </div>
+    );
+};
+
+export default LeftSideNav;
